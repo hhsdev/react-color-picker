abee2eea72c3ada5dead066d1f303c2a284bcf92
hhsdev <heinhtetsoe.dev@gmail.com>
2020-03-03 05:36:51 +0630

    Add tests for Color class

--- src/color.js
+++ src/color.js
@@ -11,68 +11,72 @@ export default class Color {
 
   static from = (color, format) => {
     switch (format) {
-    case "hexcode":
-      return Color.fromHexcode(color);
-    case "keyword":
-      return Color.fromKeyword(color);
-    case "rgb":
-      return Color.fromRgbFunction(color);
-    case "rgba":
-      return Color.fromRgbaFunction(color);
-    case "color":
-      return color;
-    case "invalid":
-      console.trace("Error: invalid format"); return null;
-    default:
-      console.trace("Error: unknown format: " + format);
-      return null;
+      case "hexcode":
+        return fromHexcode(color);
+      case "keyword":
+        return fromKeyword(color);
+      case "rgb":
+        return fromRgbFunction(color);
+      case "rgba":
+        return fromRgbaFunction(color);
+      case "color":
+        return color;
+      case "invalid":
+        console.trace("Error: invalid format");
+        return null;
+      default:
+        console.trace("Error: unknown format: " + format);
+        return null;
     }
   };
-
-  static fromKeyword = (keyword) => {
-    return Color.fromHexcode(cssColorKeywords[keyword]);
-  }
-
-  static fromHexcode = (hexcode) => {
-    hexcode = hexString.trim();
-    if (hexcode.startsWith("#")) hexString = hexString.substr(1);
-
-    if (hexcode.length === 6) {
-      const r = parseInt(hexcode.substr(0, 2), 16);
-      const g = parseInt(hexcode.substr(2, 2), 16);
-      const b = parseInt(hexcode.substr(4, 2), 16);
-
-      return Color({ r, g, b });
-    } else if (hexcode.length == 3) {
-      const r = parseInt(hexcode.charAt(0).repeat(2), 16);
-      const g = parseInt(hexcode.charAt(1).repeat(2), 16);
-      const b = parseInt(hexcode.charAt(2).repeat(2), 16);
-
-      return Color({ r, g, b });
-    } else {
-      // TODO: ADD PROPER ERROR HANDLING
-      console.trace("Error: unrecognized hex string format: " + hexcode);
-      return null;
-    }
-  }
-
-  static fromRgbFunction = (str) => {
-    const rgbMatcher = /\s*rgb\s*\(\s*(\w+)\s*,\s*(\w+)\s*,\s*(\w+)\s*\)\s*/;
-    const matches = str.match(rgbMatcher);
-    if (matches.length === 3) {
-      const [ r, g, b ] = matches.map(v => utils.baseAwareConvert(v));
-      return new Color(r, g, b, 1);
-    }
-  }
-
-  static fromRgbaFunciton = (keyword) => {
-    const rgbaMatcher = /\s*rgb\s*\(\s*(\w+)\s*,\s*(\w+)\s*,\s*(\w+)\s*\)\s*/;
-    const matches = str.match(rgbMatcher);
-    if (matches.length === 4) {
-      const [ r, g, b ] = matches.map(v => utils.baseAwareConvert(v));
-      const a = parseFloat(matches[3]);
-      return new Color(r, g, b, a);
-    }
-
-  }
 }
+
+const fromKeyword = keyword => {
+  return fromHexcode(cssColorKeywords[keyword]);
+};
+
+const fromHexcode = hexcode => {
+  hexcode = hexcode.trim();
+  if (hexcode.startsWith("#")) hexcode = hexcode.substr(1);
+
+  if (hexcode.length === 6) {
+    const r = parseInt(hexcode.substr(0, 2), 16);
+    const g = parseInt(hexcode.substr(2, 2), 16);
+    const b = parseInt(hexcode.substr(4, 2), 16);
+
+    return new Color(r, g, b, 1);
+  } else if (hexcode.length == 3) {
+    const r = parseInt(hexcode.charAt(0).repeat(2), 16);
+    const g = parseInt(hexcode.charAt(1).repeat(2), 16);
+    const b = parseInt(hexcode.charAt(2).repeat(2), 16);
+
+    return new Color(r, g, b, 1);
+  } else {
+    // TODO: ADD PROPER ERROR HANDLING
+    console.trace("Error: unrecognized hex string format: " + hexcode);
+    return null;
+  }
+};
+
+const fromRgbFunction = str => {
+  const rgbMatcher = /\s*rgb\s*\(\s*(\w+)\s*,\s*(\w+)\s*,\s*(\w+)\s*\)\s*/;
+  const matches = str.match(rgbMatcher);
+  if (matches) {
+    const [_, r, g, b] = matches.map(v => utils.baseAwareConvert(v));
+    return new Color(r, g, b, 1);
+  }
+  console.trace("Error: given string not a valid css rgb() function: " + str);
+  return null;
+};
+
+const fromRgbaFunction = str => {
+  const rgbaMatcher = /\s*rgba\s*\(\s*(\w+)\s*,\s*(\w+)\s*,\s*(\w+)\s*,\s*([0-9.]+)\s*\)\s*/;
+  const matches = str.match(rgbaMatcher);
+  if (matches) {
+    const [_, r, g, b] = matches.map(v => utils.baseAwareConvert(v));
+    const a = parseFloat(matches[4]);
+    return new Color(r, g, b, a);
+  }
+  console.trace("Error: given string not a valid css rgba() function: " + str);
+  return null;
+};
---
+++ test/color.test.js
@@ -0,0 +1,87 @@
+import { equal } from "assert";
+import Color from "../src/color";
+
+describe("Color", () => {
+  describe("#from()", () => {
+    it("should return a color object for hexcode formats", () => {
+      test_from({
+        format: "hexcode",
+        color: "#ffffff",
+        expected: new Color(0xff, 0xff, 0xff, 1)
+      });
+      test_from({
+        format: "hexcode",
+        color: "#000",
+        expected: new Color(0, 0, 0, 1)
+      });
+    });
+    it("should return a color object with correct values for keyword formats", () => {
+      test_from({
+        format: "keyword",
+        color: "white",
+        expected: new Color(0xff, 0xff, 0xff, 1)
+      });
+      test_from({
+        format: "keyword",
+        color: "black",
+        expected: new Color(0, 0, 0, 1)
+      });
+
+    });
+    it("should return a color object with correct values for rgb function formats", () => {
+      test_from({
+        format: "rgb",
+        color: "rgb(0xff, 255, 255)",
+        expected: new Color(0xff, 0xff, 0xff, 1)
+      });
+      test_from({
+        format: "rgb",
+        color: "rgb(0,0,0)",
+        expected: new Color(0, 0, 0, 1)
+      });
+
+    });
+    it("should return a color object with correct values for rgba function formats", () => {
+      test_from({
+        format: "rgba",
+        color: "rgba(0xff, 255, 255, 0.5)",
+        expected: new Color(0xff, 0xff, 0xff, 0.5)
+      });
+      test_from({
+        format: "rgba",
+        color: "rgba(0,0,0,0)",
+        expected: new Color(0, 0, 0, 0)
+      });
+
+    });
+  });
+});
+
+const color_equal = (color1, color2) => {
+  equal(color1.r, color2.r);
+  equal(color1.g, color2.g);
+  equal(color1.b, color2.b);
+  equal(color1.a, color2.a);
+};
+
+const test_from = test_case => {
+  const { color, format, expected } = test_case;
+  const actual = Color.from(color, format);
+  color_equal(expected, actual);
+};
+
+const test_from_with_format_hexcode = fixtures => {
+  for (const fixture of fixtures) {
+    const { expected, hexcode } = fixture;
+    const actual = Color.from(keyword, "keyword");
+    color_equal(expected, actual);
+  }
+};
+
+const test_from_with_format_keyword = fixtures => {
+  for (const fixture of fixtures) {
+    const { expected, keyword } = fixture;
+    const actual = Color.from(keyword, "keyword");
+    color_equal(expected, actual);
+  }
+};
