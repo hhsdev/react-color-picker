ef2a8dc1f5ead1e9bfc10b2f3e6213f7bdec3395
hhsdev <heinhtetsoe.dev@gmail.com>
2020-04-12 18:35:21 +0630

    Add useTouch.js
    useTouch.js is a custom react hook for handling touch related events.

---
+++ src/useTouch.js
@@ -0,0 +1,103 @@
+import React, { useRef, useEffect, useCallback } from "react";
+
+export default function useTouch(eventRef, callback) {
+  const touchId = useRef();
+
+  const trackFingerPosition = (event, touchId) => {
+    // mozilla has a bug in responsive design mode where event.changedTouches
+    // are empty on a touchend event:
+    //    https://bugzilla.mozilla.org/show_bug.cgi?id=1615824
+    // as a workaround, this currently returns true on a touchend event.
+    // Delete this after the bug has been resolved!
+    if (event.type === "touchend" && event.changedTouches.length === 0) {
+      return true;
+    }
+
+    if (touchId.current !== undefined && event.changedTouches) {
+      for (let i = 0; i < event.changedTouches.length; i += 1) {
+        const touch = event.changedTouches[i];
+        if (touch.identifier === touchId.current) {
+          return {
+            x: touch.clientX,
+            y: touch.clientY,
+          };
+        }
+      }
+      return false;
+    }
+    return {
+      x: event.clientX,
+      y: event.clientY,
+    };
+  };
+
+  const handleTouchStart = (event) => {
+    event.preventDefault();
+    const touch = event.changedTouches[0];
+    if (touch != null) {
+      touchId.current = touch.identifier;
+    }
+
+    const fingerPosition = trackFingerPosition(event, touchId);
+    callback(fingerPosition);
+
+    document.addEventListener("touchmove", handleTouchMove);
+    document.addEventListener("touchend", handleTouchEnd);
+  };
+
+  const handleTouchMove = (event) => {
+    const finger = trackFingerPosition(event, touchId);
+    if (!finger) return;
+    callback(finger);
+  };
+
+  const handleTouchEnd = (event) => {
+    touchId.current = undefined;
+
+    const fingerPosition = trackFingerPosition(event, touchId);
+
+    if (!fingerPosition) return;
+    document.removeEventListener("mousemove", handleTouchMove);
+    document.removeEventListener("mouseup", handleTouchEnd);
+    document.removeEventListener("touchmove", handleTouchMove);
+    document.removeEventListener("touchend", handleTouchEnd);
+  };
+
+  useEffect(() => {
+    if (!eventRef.current) return;
+    eventRef.current.addEventListener("touchstart", handleTouchStart);
+    eventRef.current.addEventListener("mousedown", handleMouseDown);
+
+    return () => {
+      eventRef.current.removeEventListener("mousedown", handleMouseDown);
+      eventRef.current.removeEventListener("touchstart", handleTouchStart);
+      document.removeEventListener("mousemove", handleTouchMove);
+      document.removeEventListener("mouseup", handleTouchEnd);
+      document.removeEventListener("touchmove", handleTouchMove);
+      document.removeEventListener("touchend", handleTouchEnd);
+    };
+  }, [eventRef, handleTouchEnd, handleTouchMove, handleTouchStart]);
+
+  const handleMouseDown = (e) => {
+    callback({ x: e.clientX, y: e.clientY });
+    document.body.style["pointer-events"] = "none";
+
+    document.addEventListener("mouseup", handleMouseUp, { capture: true });
+    document.addEventListener("mousemove", handleMouseMove, { capture: true });
+    e.stopPropagation();
+  };
+
+  const handleMouseUp = (e) => {
+    document.body.style["pointer-events"] = "auto";
+    document.removeEventListener("mouseup", handleMouseUp, { capture: true });
+    document.removeEventListener("mousemove", handleMouseMove, {
+      capture: true,
+    });
+    e.stopPropagation();
+  };
+
+  const handleMouseMove = (e) => {
+    callback({ x: e.clientX, y: e.clientY });
+    e.stopPropagation();
+  };
+}
